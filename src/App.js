c7d046057d53f587fa6af43ac0375613cf0db7b8
Mark Emmanuel Teodoro <mnteodoro@up.edu.ph>
2023-01-29 19:55:49 +0800

    added a few changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Header from '../src/components/Header'
+import Nav from '../src/components/Nav'
+import Main from '../src/components/Main'
+import Footer from '../src/components/Footer'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Header></Header>
+      <Nav></Nav>
+      <Main></Main>
+      <Footer></Footer>
+    </>
   );
 }
 
